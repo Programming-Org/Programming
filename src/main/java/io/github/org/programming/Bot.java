a2887eb46e19e37cd12e222d79eaf4c6ef84e561
Yusuf Arfan Ismail <yusufgamer222@gmail.com>
2022-06-05 14:32:58 +0100

    Update Bot.java

--- src/main/java/io/github/org/programming/Bot.java
+++ src/main/java/io/github/org/programming/Bot.java
@@ -1,10 +1,6 @@
 package io.github.org.programming;
 
 import io.github.org.programming.bot.ProgrammingBot;
-import net.dv8tion.jda.api.JDA;
-import net.dv8tion.jda.api.JDABuilder;
-
-import javax.security.auth.login.LoginException;
 
 public class Bot {
     public static void main(String[] args) throws Exception {
