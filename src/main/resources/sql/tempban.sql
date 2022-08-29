d7cfe816a02a32ef6ac968545a8425fee3f3cf46
Yusuf Arfan Ismail <yusufgamer222@gmail.com>
2022-08-29 20:56:32 +0100

    cleaning up class

--- src/main/resources/sql/tempban.sql
+++ src/main/resources/sql/tempban.sql
@@ -4,7 +4,7 @@ CREATE TABLE IF NOT EXISTS tempban
     guild_id     varchar(255) not null,
     user_id      varchar(255) not null,
     moderator_id varchar(255) not null,
-    duration     interval         not null,
+    duration     interval     not null,
     start        timestamp    not null,
     reason       varchar(255) not null
 )
