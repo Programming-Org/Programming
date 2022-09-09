d29023c41664d9c8be5a9c97277100c4cfaccc3e
Yusuf Arfan Ismail <yusufgamer222@gmail.com>
2022-09-09 16:11:22 +0100

    starting work on help command

---
+++ src/main/java/io/github/org/programming/bot/commands/help/HelpCommand.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2022 Programming Org and other Programming Org contributors
+ *
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ *
+ * you may not use this file except in compliance with the License.
+ *
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */ 
+package io.github.org.programming.bot.commands.help;
+
+import io.github.org.programming.backend.builder.slash.SlashCommand;
+import io.github.org.programming.backend.builder.slash.SlashCommandBuilder;
+import io.github.org.programming.backend.extension.SlashCommandExtender;
+import io.github.org.programming.backend.type.CommandType;
+import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
+
+public class HelpCommand implements SlashCommandExtender {
+    private final List<SlashCommandExtender> commands;
+
+    public HelpCommand(List<SlashCommandExtender> commands) {
+        this.commands = commands;
+    }
+
+    @Override
+    public void onSlashCommandInteraction(@NotNull SlashCommandInteractionEvent event) {
+        commands.forEach(c -> {
+            SlashCommand command = c.build();
+            CommandType type = command.getCommandType();
+        });
+    }
+
+    @Override
+    public SlashCommand build() {
+        return new SlashCommandBuilder("help", "Used to get help").build().setToGuildOnly();
+    }
+}
