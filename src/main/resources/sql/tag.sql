ff1c82a33fd735694493f53d3e8470c58727a642
Yusuf Arfan Ismail <yusufgamer222@gmail.com>
2022-09-12 17:08:51 +0100

    cleanup

--- src/main/resources/sql/tag.sql
+++ src/main/resources/sql/tag.sql
@@ -1,6 +1,7 @@
-CREATE TABLE IF NOT EXISTS tag (
-    id varchar(255) NOT NULL,
-    name varchar(255) NOT NULL,
+CREATE TABLE IF NOT EXISTS tag
+(
+    id          varchar(255) NOT NULL,
+    name        varchar(255) NOT NULL,
     description varchar(999) NOT NULL,
     PRIMARY KEY (id)
 )
