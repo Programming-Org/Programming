a29f1375ea1befa6cf8c4d1764fdc82938ae571e
Yusuf Arfan Ismail <yusufgamer222@gmail.com>
2022-09-13 07:35:51 +0100

    Finished with tag command
    need to test it

--- src/main/java/io/github/org/programming/bot/tag/TagCommand.java
+++ src/main/java/io/github/org/programming/bot/tag/TagCommand.java
@@ -22,8 +22,13 @@ import io.github.org.programming.backend.builder.slash.SlashCommand;
 import io.github.org.programming.backend.builder.slash.SlashCommandBuilder;
 import io.github.org.programming.backend.extension.SlashCommandExtender;
 import io.github.org.programming.backend.type.CommandType;
+import io.github.org.programming.bot.commands.util.GuildOnlyCommand;
+import io.github.org.programming.database.tag.TagDatabase;
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.Permission;
 import net.dv8tion.jda.api.events.interaction.ModalInteractionEvent;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+import net.dv8tion.jda.api.interactions.commands.OptionMapping;
 import net.dv8tion.jda.api.interactions.commands.OptionType;
 import net.dv8tion.jda.api.interactions.commands.build.SubcommandData;
 import net.dv8tion.jda.api.interactions.components.ActionRow;
@@ -32,9 +37,13 @@ import net.dv8tion.jda.api.interactions.components.text.TextInput;
 import net.dv8tion.jda.api.interactions.components.text.TextInputStyle;
 import org.jetbrains.annotations.NotNull;
 
+import java.awt.*;
 import java.util.Objects;
 
 public class TagCommand extends SlashCommandExtender {
+    private String newTageID;
+    private String editTagID;
+
     @Override
     public void onSlashCommand(@NotNull SlashCommandInteractionEvent event) {
         switch (Objects.requireNonNull(event.getSubcommandName())) {
@@ -46,13 +55,32 @@ public class TagCommand extends SlashCommandExtender {
         }
     }
 
-    private void onTagGet(@NotNull SlashCommandInteractionEvent event) {}
+    private void onTagGet(@NotNull SlashCommandInteractionEvent event) {
+        String tagId = Objects.requireNonNull(event.getOption("tag_id")).getAsString();
+
+        String tagName = TagDatabase.getName(tagId);
+        String tagDescription = TagDatabase.getDescription(tagId);
+
+        var embedBuilder = new EmbedBuilder().setTitle(tagName)
+            .setDescription(tagDescription)
+            .setColor(Color.GRAY)
+            .setFooter("Tag ID: " + tagId)
+            .setTimestamp(event.getTimeCreated())
+            .build();
+
+        event.replyEmbeds(embedBuilder).setEphemeral(true).queue();
+    }
 
     private void onTagCreate(@NotNull SlashCommandInteractionEvent event) {
+        newTageID = event.getOption("tag_id", OptionMapping::getAsString);
+        GuildOnlyCommand.guildOnlyCommand(event);
+        checkForPerms(event);
+
         TextInput tagName = TextInput.create("tag-name", "Tag Name", TextInputStyle.SHORT)
             .setPlaceholder("Enter the name of the tag")
             .setMinLength(1)
             .setMaxLength(32)
+            .setRequired(true)
             .build();
 
         TextInput tagDescription =
@@ -60,6 +88,7 @@ public class TagCommand extends SlashCommandExtender {
                     .setPlaceholder("Enter the description of the tag")
                     .setMinLength(30)
                     .setMaxLength(999)
+                    .setRequired(true)
                     .build();
 
         Modal modal = Modal.create("tag-create", "Create A Tag")
@@ -69,14 +98,74 @@ public class TagCommand extends SlashCommandExtender {
         event.replyModal(modal).queue();
     }
 
-    @Override
-    public void onModalInteraction(@NotNull ModalInteractionEvent event) {
+    private void onTagEdit(@NotNull SlashCommandInteractionEvent event) {
+        GuildOnlyCommand.guildOnlyCommand(event);
+        checkForPerms(event);
+        editTagID = event.getOption("tag_id", OptionMapping::getAsString);
 
+        TextInput tagName = TextInput.create("tag-name", "New Tag Name", TextInputStyle.SHORT)
+            .setPlaceholder("Enter the name of the tag")
+            .setMinLength(1)
+            .setMaxLength(32)
+            .setRequired(false)
+            .build();
+
+        TextInput tagDescription =
+                TextInput.create("tag-description", "New Tag Description", TextInputStyle.PARAGRAPH)
+                    .setPlaceholder("Enter the description of the tag")
+                    .setMinLength(30)
+                    .setMaxLength(999)
+                    .setRequired(false)
+                    .build();
     }
 
-    private void onTagEdit(@NotNull SlashCommandInteractionEvent event) {}
+    private void onTagDelete(@NotNull SlashCommandInteractionEvent event) {
+        GuildOnlyCommand.guildOnlyCommand(event);
+        checkForPerms(event);
 
-    private void onTagDelete(@NotNull SlashCommandInteractionEvent event) {}
+        TagDatabase.deleteTag(event.getOption("tag_id", OptionMapping::getAsString));
+
+        event.reply("Tag deleted").setEphemeral(true).queue();
+    }
+
+    private void checkForPerms(SlashCommandInteractionEvent event) {
+        if (!event.getMember().hasPermission(Permission.MANAGE_SERVER)) {
+            event.reply("You do not have permission to use this command")
+                .setEphemeral(true)
+                .queue();
+            return;
+        }
+
+        if (!event.getGuild().getSelfMember().hasPermission(Permission.MANAGE_SERVER)) {
+            event.reply("I do not have permission to use this command").setEphemeral(true).queue();
+            return;
+        }
+    }
+
+    @Override
+    public void onModalInteraction(@NotNull ModalInteractionEvent event) {
+        System.out.println(event.getModalId());
+        if (event.getModalId().equals("tag-create")) {
+            TagDatabase.createTag(newTageID, event.getValue("tag-name").getAsString(),
+                    event.getValue("tag-description").getAsString());
+            event.getInteraction().reply("Tag created").setEphemeral(true).queue();
+        }
+
+        if (event.getModalId().equals("tag-edit")) {
+            if (event.getValue("tag-name") != null) {
+                TagDatabase.editTagName(editTagID, event.getValue("tag-name").getAsString());
+            } else if (event.getValue("tag-description") != null) {
+                TagDatabase.editTagDescription(editTagID,
+                        event.getValue("tag-description").getAsString());
+            } else {
+                event.getInteraction()
+                    .reply("You must enter a new name or description")
+                    .setEphemeral(true)
+                    .queue();
+            }
+        }
+    }
+
 
     @Override
     public SlashCommand build() {
---
+++ src/main/java/io/github/org/programming/database/tag/TagDatabase.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright 2022 Programming Org and other Programming Org contributors
+ *
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ *
+ * you may not use this file except in compliance with the License.
+ *
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */ 
+package io.github.org.programming.database.tag;
+
+import static io.github.org.programming.bot.ProgrammingBot.getContext;
+import static io.github.org.programming.jooq.Tables.TAG;
+
+public class TagDatabase {
+    public static void createTag(String tagId, String tagName, String tagDescription) {
+        getContext().insertInto(TAG, TAG.ID, TAG.NAME, TAG.DESCRIPTION)
+            .values(tagId, tagName, tagDescription)
+            .execute();
+    }
+
+    public static String getName(String tagId) {
+        return getContext().select(TAG.NAME).from(TAG).where(TAG.ID.eq(tagId)).fetchOne(TAG.NAME);
+    }
+
+    public static String getDescription(String tagId) {
+        return getContext().select(TAG.DESCRIPTION)
+            .from(TAG)
+            .where(TAG.ID.eq(tagId))
+            .fetchOne(TAG.DESCRIPTION);
+    }
+
+    public static void editTagName(String tagId, String tagName) {
+        getContext().update(TAG).set(TAG.NAME, tagName).where(TAG.ID.eq(tagId)).execute();
+    }
+
+    public static void editTagDescription(String tagId, String tagDescription) {
+        getContext().update(TAG)
+            .set(TAG.DESCRIPTION, tagDescription)
+            .where(TAG.ID.eq(tagId))
+            .execute();
+    }
+
+    public static void deleteTag(String tagId) {
+        getContext().deleteFrom(TAG).where(TAG.ID.eq(tagId)).execute();
+    }
+}
